873fee142df9e69db16d4a49427c28e11dee2ca9
Yanxin <42981100+yanxin0124@users.noreply.github.com>
2020-04-28 13:00:00 -0400

    p5js.play

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//** Variables **********
+// var playerIdle1 = "images/playerIdle1.png";
+// var playerIdle2 = "images/playerIdle2.png";
+// var playerIdle3 = "images/playerIdle3.png";
+//var playerIdle;
+
+function preload() {
+  //let test = loadImage("images/playerIdle1.png");
+  //playerIdle = loadAnimation(test);
+  //playerIdle = loadAnimation(playerIdle1, playerIdle2, playerIdle3);
+	//playerIdle = loadAnimation(playerIdle1, playerIdle2, playerIdle1, playerIdle3, playerIdle1, playerIdle3);
+}
+
+//** Canvas *************
+function setup() {
+	createCanvas(window.innerWidth, window.innerHeight);
+
+} // function setup
+
+//** Draw ****************
+function draw() {
+	document.body.style.backgroundImage = "url('images/blueSky.png')";
+
+
+
+  //image(test, window.innerWidth/2, window.innerHeight/2);
+	//animation(playerIdle, window.innerWidth/2, window.innerHeight/2);
+
+} // function draw
