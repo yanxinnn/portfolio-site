81cbca539ff6aad6e679764e3e781cea4c18175b
Yanxin <42981100+yanxin0124@users.noreply.github.com>
2020-05-01 20:43:32 -0400

    tint

--- sketch.js
+++ sketch.js
@@ -59,15 +59,13 @@ function setup() {
 
   // Widgets
   //Fullscreen
+  tint(255, 200);
   fullscreen = createSprite(window.innerWidth * 23 / 25, window.innerHeight / 18, 30, 30);
   fullscreen.addAnimation("isOff", fullscreenIsOff);
   fullscreen.addAnimation("isOn", fullscreenIsOn);
   fullscreen.changeAnimation("isOff");
   widget.onMouseOver = function () {
     tint(255, 126);
-  }
-  else {
-    tint(255, 200);
   }
   fullscreen.onMousePressed = function() {
     if (fullscreen.getAnimationLabel() == "isOff") {
