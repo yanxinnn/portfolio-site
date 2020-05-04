87041eb099975f125ca248a31f4c9335b90d16c9
Yanxin <42981100+yanxin0124@users.noreply.github.com>
2020-05-04 19:58:50 -0400

    take away ground

--- sketch.js
+++ sketch.js
@@ -37,8 +37,8 @@ function preload() {
   snd_windyPetals = loadSound("sounds/windyPetals.mp3");
 
   // Environment
-  grass = loadAnimation("images/environment/grass.png");
-  dirt = loadAnimation("images/environment/dirt.png");
+  //grass = loadAnimation("images/environment/grass.png");
+  //dirt = loadAnimation("images/environment/dirt.png");
 
 }
 
@@ -57,17 +57,17 @@ function setup() {
   player.addAnimation("run", playerRun);
 
   // Environment
-  platforms = new Group();
-  for (i = 0; i < 15; ++i) {
-    ground = createSprite(i * 100, window.innerHeight - 200, 100, 100);
-    ground.addAnimation("grass", grass);
-    ground.addToGroup(platforms);
-  }
-  for (i = 0; i < 15; ++i) {
-    ground = createSprite(i * 100, window.innerHeight - 100, 100, 100);
-    ground.addAnimation("dirt", dirt);
-    ground.addToGroup(platforms);
-  }
+  // platforms = new Group();
+  // for (i = 0; i < 18; ++i) {
+  //   ground = createSprite(i * 100, window.innerHeight - 200, 100, 100);
+  //   ground.addAnimation("grass", grass);
+  //   ground.addToGroup(platforms);
+  // }
+  // for (i = 0; i < 18; ++i) {
+  //   ground = createSprite(i * 100, window.innerHeight - 100, 100, 100);
+  //   ground.addAnimation("dirt", dirt);
+  //   ground.addToGroup(platforms);
+  // }
 
 } // function setup
 
@@ -100,7 +100,7 @@ function draw() {
     player.velocity.y = 5;
   }
 
-  // Player Ground Collision
+  //Player Ground Collision
  //  if (ground.overlapPixel(player.position.x, player.position.y+100)==false) {
  //    player.velocity.y += 0.5;
  //  }
@@ -109,7 +109,7 @@ function draw() {
  //   player.velocity.y = 0;
  // }
 
- drawSprites(platforms);
+ //drawSprites(platforms);
  drawSprites(player);
 
 } // function draw
